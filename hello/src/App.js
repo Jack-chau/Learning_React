18379837ef20f8f9c1d22ad95c230b52d3f44c15
JackChau <chaukaho149@gmail.com>
2022-12-08 07:28:48 +0800

    first cmponent

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  console.log ( 'we are about to list the employees' ) ;
+  const showEmployees = false ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { showEmployees ? (
+          <>
+            <Employee />
+            <Employee />
+            <Employee />
+            <Employee />
+            <Employee />
+          </>
+        ) : (
+          <p>You cannot see the employees</p>
+        )
+      }
     </div>
   );
 }
-
 export default App;
---
+++ hello/src/components/Employee.js
@@ -0,0 +1,5 @@
+function Employee () {
+    return <h6>Here is an employee</h6>
+}
+
+export default Employee;
