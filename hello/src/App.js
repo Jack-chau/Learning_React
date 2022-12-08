4605005a9afd3864323ce314c8e90c9e93cf3fb4
JackChau <chaukaho149@gmail.com>
2022-12-08 21:40:09 +0800

    getting start or css

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role,setRole] = useState('dev');
   console.log ( 'we are about to list the employees' ) ;
-  const showEmployees = false ;
+  const showEmployees = true ;
   return (
     <div>
       { showEmployees ? (
           <>
-            <Employee />
-            <Employee />
-            <Employee />
-            <Employee />
-            <Employee />
+          <input 
+            type='text' 
+            onChange={(e) => {
+              console.log( e.target.value ); // 'e.target.value' to get user input
+                setRole( e.target.value ); 
+          }}/>
+            <Employee name = "Jack" role = "Intern"/>
+            <Employee name = "Nicole" role = "Professor"/>
+            <Employee name = "Jerffy" role={role}/>
+            <Employee name = "Rojer"/>
+            <Employee name =  "Donnal"/>
           </>
         ) : (
           <p>You cannot see the employees</p>
