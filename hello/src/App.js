6a41074db01add23e799dff80eceea57609e5cc1
JackChau <chaukaho149@gmail.com>
2022-12-15 20:06:10 +0800

    add callback function and modle

--- hello/src/App.js
+++ hello/src/App.js
@@ -4,42 +4,60 @@ import {useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 function App() {
-  const [role,setRole] = useState('football athlete');
-  const [employees, setEmployee] = useState (  // Multiple employee inside an array
+  const [ role, setRole ] = useState ( 'football athlete' ) ;
+  const [ employees, setEmployees ] = useState (  // Multiple employee inside an array
     [
       {
+        id : 1,
         name: "realJack",
         role: "Trainee",
         img: "https://images.pexels.com/photos/2970497/pexels-photo-2970497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       },
       {
+        id : 2,
         name: "HappyJack",
         role: "Grabish",
         img: "https://i.ytimg.com/vi/fkorWPoi6E0/maxresdefault.jpg",
       },
       {
+        id : 3,
         name: "MotherFucker",
         role: "football athlete",
         img: "https://i2-prod.dailystar.co.uk/incoming/article28704237.ece/ALTERNATES/s1200c/0_Mbappe-was-laughing-after-Kane-missed-the-penalty.jpg",
       },
       {
+        id : 4,
         name: "guessWho",
         role: "Killer",
         img: "https://images.pexels.com/photos/6471231/pexels-photo-6471231.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       },
       {
+        id : 5,
         name: "whoAmI",
         role: "eco fans",
         img: "https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       },
       {
+        id : 6,
         name: "Jesus",
         role: "I am God",
         img: "https://john832knowthetruth.org/wp-content/uploads/2020/12/cross02-1184x395.jpg",
       }
     ]
-  )
+  );
+  
+  function updateEmployee ( id, newName, newRole ) {
+      const updateEmployee = employees.map ( ( employee ) => {
+        if ( id == employee.id ) {
+          return {...employee, name: newName, role: newRole } ; 
+        }
+        return employee ;
+      } );
+    setEmployees ( updateEmployee ) ;
+  }
+
   console.log ( 'we are about to list the employees' ) ;
+
   const showEmployees = true ;
   return (
     <div className = "App">
@@ -56,10 +74,12 @@ function App() {
                 console.log(employee);
                 return (
                   <Employee
-                    key = { uuidv4() }
+                    key = { employee.id }
+                    id = { employee.id }
                     name = { employee.name } 
                     role = { employee.role } 
                     img = { employee.img }
+                    updateEmployee = { updateEmployee }
                   />
                 );
               })}
--- hello/src/components/EditEmployee.js
+++ hello/src/components/EditEmployee.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
-  const [show, setShow] = useState(false);
+function EditEmployee( props ) {
+    const [ name, setName ] = useState ( props.name ) ;
+    const [ role, setRole ] = useState ( props.role ) ;
+    
+    const [ show, setShow ] = useState ( false ) ;   
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const handleClose = ( ) => setShow ( false ) ;
+    const handleShow = ( ) => setShow ( true ) ;
 
-  return (
+    return (
     <>
         <button
             onClick = { handleShow }
@@ -26,39 +29,52 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <form id="editmodal" className="w-full max-w-sm">
+        <form 
+            onSubmit = {(e) => {
+            handleClose ( ) ;
+            e.preventDefault ( ) ;
+            console.log ( "hello from edit Employee" ) ;
+            console.log ( props.id, name, role ) ;
+            props.updateEmployee ( props.id, name, role ) ;
+        }}
+        id="editmodal" className="w-full max-w-sm">
             <div className="md:flex md:items-center mb-6">
                 <div className="md:w-1/3">
                 <label 
-                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" 
+                    className="block text-gray-500 font-bold md:text-right mb- 1 md:mb-0 pr-4" 
                     for="name">
                     Full Name
                 </label >
                 </div>
-                <div className="md:w-2/3">
+                <div className = "md:w-2/3">
                 <input 
-                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="inline-full-name" 
-                    type="text" 
-                    value="Jane Doe" 
+                    className = "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                    id = "inline-full-name" 
+                    type = "text" 
+                    value = {name}
+                    onChange = {(e) => {
+                        setName(e.target.value)}}
                 />
                 </div>
             </div>
 
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
+            <div className = "md:flex md:items-center mb-6">
+                <div className = "md:w-1/3">
                 <label 
-                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" 
-                    for="role">
+                    className = "block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" 
+                    for = "role">
                     Role
                 </label >
                 </div>
-                <div className="md:w-2/3">
+                <div className = "md:w-2/3">
+
                 <input 
-                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="role" 
-                    type="text" 
-                    value="Jane Doe" 
+                    className = "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                    id = "role" 
+                    type = "text" 
+                    value = { role }
+                    onChange = {(e) => {
+                        setRole(e.target.value)}}
                 />
                 </div>
             </div>
@@ -68,11 +84,11 @@ function EditEmployee() {
         <Modal.Footer>
             <button 
                 className = "bg-gray-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
-                onClick = {handleClose}
+                onClick = { handleClose }
             >
                 Close
             </button>
-            <button 
+            <button
                 className = "bg-purple-600 hover:bg-purple-7 00 text-white font-bold py-2 px-4 rounded"
                 form="editmodal">
                 Update
